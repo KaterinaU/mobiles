4d4ad736c3aecbeca94c0370530bb08359ee360f
Ekaterina <ushakova.esu@gmail.com>
2024-11-18 01:35:41 +0500

    feature: add Header and styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,1 +1,16 @@
-export {};
+import * as React from 'react';
+import './styles.scss';
+import { Person } from '../../assets/icons/Person';
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="catalog">Каталог</div>
+      <div className="account">
+        <div>СРАВНЕНИЕ</div>
+        <div>Личный кабинет</div>
+        <Person className="icons" />
+      </div>
+    </div>
+  );
+};
