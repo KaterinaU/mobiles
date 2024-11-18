d0912b7ad31e74fbee2f3c2aef4bdf6d5596d02c
Ekaterina <ushakova.esu@gmail.com>
2024-11-19 01:40:40 +0500

    fix: change Pagination and styles

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,24 @@
-export {};
+import React from 'react';
+import './paginationStyles.scss';
+
+export type PaginationType = {
+  totalPages: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination: React.FC<PaginationType> = ({ totalPages, currentPage, onPageChange }) => {
+  return (
+    <div className="pagination-container">
+      {[...Array(totalPages)].map((_, index) => (
+        <span
+          key={index}
+          onClick={() => onPageChange(index + 1)}
+          className={`pagination-number ${currentPage === index + 1 ? 'active' : ''}`}
+        >
+          {index + 1}
+        </span>
+      ))}
+    </div>
+  );
+};
