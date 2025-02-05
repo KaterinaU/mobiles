eaa19c937192d3d6bf51c0a8876880cba99087f7
Ekaterina <ushakova.esu@gmail.com>
2025-02-05 21:43:16 +0500

    fix: add rtk to MainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 import { Table } from '../../components/Table';
@@ -10,29 +9,21 @@ import { PhoneReplaceContent } from '../../components/Modal/components/PhoneRepl
 import { PhoneType } from '../../types';
 
 import styles from './styles.module.scss';
+import { replacePhones } from '../../redux/slices/phone/phoneSlice';
+import { PhoneService } from '../../api/services/PhoneServise/PhoneServise';
+import { useAppDispatch } from '../../redux/store';
 
 export const MainPage = () => {
-  const [phones, setPhones] = useState<PhoneType[]>([]);
-  const [itemsToShow, setItemsToShow] = useState(3);
+  const dispatch = useAppDispatch();
+
   const [isModalVisible, setModalVisible] = useState(false);
   const [selectedPhoneId, setSelectedPhoneId] = useState<number | null>(null);
   const [showDifferences, setShowDifferences] = useState(false);
   const [modalPosition, setModalPosition] = useState<{ top: number; left: number }>({ top: 0, left: 0 });
 
   useEffect(() => {
-    axios
-      .get('/data/phones.json')
-      .then((response) => {
-        setPhones(response.data);
-      })
-      .catch((error) => {
-        console.error('Ошибка при загрузке данных:', error);
-      });
+    dispatch(PhoneService.getPhones());
   }, []);
-
-  const maxItemsToShow = Math.min(phones.length, 6);
-  const phonesToReplace = phones.slice(itemsToShow);
-  const visiblePhones = phones.slice(0, itemsToShow);
 
   const handleChevronClick = (id: number) => {
     const element = document.getElementById(`phone-${id}`);
@@ -41,33 +32,21 @@ export const MainPage = () => {
       const rect = element.getBoundingClientRect();
 
       setModalPosition({
-        top: rect.top + window.scrollY + rect.height / 4, // наполовину перекрывает телефон
-        left: rect.left + window.scrollX + rect.width / 2, // центрируем по горизонтали
+        top: rect.top + window.scrollY + rect.height / 4,
+        left: rect.left + window.scrollX + rect.width / 2,
       });
     } else {
       console.error(`Элемент с id="phone-${id}" не найден.`);
     }
-
     setSelectedPhoneId(id);
     setModalVisible(true);
   };
 
   const handleReplace = (newPhone: PhoneType) => {
     if (selectedPhoneId !== null) {
-      const updatedPhones = [...phones];
-      const replaceIndex = phones.findIndex((phone) => phone.id === selectedPhoneId);
-      const newIndex = phones.findIndex((phone) => phone.id === newPhone.id);
-
-      if (replaceIndex !== -1 && newIndex !== -1) {
-        [updatedPhones[replaceIndex], updatedPhones[newIndex]] = [updatedPhones[newIndex], updatedPhones[replaceIndex]];
-        setPhones(updatedPhones);
-        setModalVisible(false);
-      }
+      dispatch(replacePhones({ oldPhoneId: selectedPhoneId, newPhoneId: newPhone.id }));
+      setModalVisible(false);
     }
-  };
-
-  const handleShowItemsChange = (count: number) => {
-    setItemsToShow(Math.min(count, maxItemsToShow));
   };
 
   const handleCloseModal = () => {
@@ -76,14 +55,11 @@ export const MainPage = () => {
   return (
     <PageContainer>
       <Modal isOpen={isModalVisible} onClose={handleCloseModal} phoneId={selectedPhoneId} position={modalPosition}>
-        <PhoneReplaceContent phones={phonesToReplace} onReplace={handleReplace} />
+        <PhoneReplaceContent onReplace={handleReplace} />
       </Modal>
       <div className={styles.mainPage}>
         <div className={styles.phoneTable}>
           <Table
-            phones={visiblePhones}
-            setItemsToShow={handleShowItemsChange}
-            itemsToShow={itemsToShow}
             onChevronClick={handleChevronClick}
             showDifferences={showDifferences}
             setShowDifferences={setShowDifferences}
