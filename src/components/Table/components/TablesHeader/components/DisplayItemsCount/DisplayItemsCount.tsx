46b476f4531a33c8336fbf095cc70976e1699e2a
Ekaterina <ushakova.esu@gmail.com>
2025-02-05 21:47:10 +0500

    fix: change DisplayItemsCount

--- src/components/Table/components/TablesHeader/components/DisplayItemsCount/DisplayItemsCount.tsx
+++ src/components/Table/components/TablesHeader/components/DisplayItemsCount/DisplayItemsCount.tsx
@@ -1,39 +1,38 @@
 import React, { useState } from 'react';
+import { useAppDispatch } from '../../../../../../redux/store';
+import { setDisplayedPhonesCount } from '../../../../../../redux/slices/phone/phoneSlice';
 
 import styles from './DisplayItemsCount.module.scss';
 
 const ITEMS_COUNT_OPTIONS = [2, 3, 4, 5, 6];
 
-export type DisplayItemsCountType = {
-  onChange: (count: number) => void;
-};
+export const DisplayItemsCount = () => {
+  const dispatch = useAppDispatch();
 
-export const DisplayItemsCount = ({ onChange }: DisplayItemsCountType) => {
   const [activeCount, setActiveCount] = useState<number>(ITEMS_COUNT_OPTIONS[0]);
 
   const createButtonClickHandler = (count: number) => () => {
     setActiveCount(count);
-    onChange(count);
+    dispatch(setDisplayedPhonesCount(count));
   };
+
   return (
-    <>
-      <div className={styles.headerSmart}>
-        <div className={styles.smart}>Смартфоны</div>
-        <div className={styles.counter}>
-          <div>Отобразить товары:</div>
-          <div className={styles.displayContainer}>
-            {ITEMS_COUNT_OPTIONS.map((count) => (
-              <button
-                key={count}
-                className={`${styles.displayNumber} ${activeCount === count ? styles.active : ''}`}
-                onClick={createButtonClickHandler(count)}
-              >
-                {count}
-              </button>
-            ))}
-          </div>
+    <div className={styles.headerSmart}>
+      <div className={styles.smart}>Смартфоны</div>
+      <div className={styles.counter}>
+        <div>Отобразить товары:</div>
+        <div className={styles.displayContainer}>
+          {ITEMS_COUNT_OPTIONS.map((count) => (
+            <button
+              key={count}
+              className={`${styles.displayNumber} ${activeCount === count ? styles.active : ''}`}
+              onClick={createButtonClickHandler(count)}
+            >
+              {count}
+            </button>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
